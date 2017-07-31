5af28a37fc38be99afba49fdcf879f910159f7e7
lukyans <sukyans@gmail.com>
2017-07-31 11:42:47 -0600

    styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,29 +6,36 @@ class SessionsController < ApplicationController
   def create
     @parker = Parker.find_by(email: params[:session][:email])
     @driver = Driver.find_by(email: params[:session][:email])
-if @driver
-    if @driver && @driver.authenticate(params[:session][:password])
-      session[:driver_id] = @driver.id
-      redirect_to drivers_path
-    else
-      flash[:alert] = "invalid email or password"
-      render :new
-    end
+  if @driver
+      if @driver && @driver.authenticate(params[:session][:password])
+        session[:driver_id] = @driver.id
+        redirect_to drivers_path
+      else
+        flash[:alert] = "invalid email or password"
+        render :new
+      end
   else
-    if @parker && @parker.authenticate(params[:session][:password])
-      session[:parker_id] = @parker.id
-      redirect_to parkers_path
+      if @parker && @parker.authenticate(params[:session][:password])
+        session[:parker_id] = @parker.id
+        redirect_to parkers_path
     else
-      flash[:alert] = "invalid email or password"
-      render :new
+        flash[:alert] = "invalid email or password"
+        render :new
     end
-  end
-
-  end
-
-  def destroy
-    session.clear
-    flash[:message] = "Successful logged out."
-    redirect_to root_path
   end
 end
+
+  def destroy
+    if current_driver != nil && current_parker != nil
+      session.clear
+      flash[:message] = "Successful logged out Driver."
+      redirect_to root_path
+    elsif current_parker != nil
+      session.clear
+      flash[:message] = "Successful logged out Parker."
+      redirect_to root_path
+    end
+    else
+      flash[:alert] = "Something went wrong!"
+    end
+  end
